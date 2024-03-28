67a621856d1d3ee7721aea4048ec873e0264b603
Ashish Kumar Mishra <ashishrkl31@gmail.com>
2024-03-28 23:11:54 +0530

    Create User Update Api

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    if (!token) 
+        return next(errorHandler(401, 'You are Unauthorized!'));
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) return next(errorHandler(403, 'Token is not valid!'));
+            
+            req.user = user;
+            next();
+        });
+}
