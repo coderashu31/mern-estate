3ac3cc050c407ccd169914c91e23722a7f779a09
Ashish Kumar Mishra <ashishrkl31@gmail.com>
2024-03-30 21:08:08 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -15,7 +15,7 @@ export const updateUser = async (req, res, next) => {
       if (req.body.password) {
         req.body.password = bcryptjs.hashSync(req.body.password, 10);
       }
-
+ 
       const updatedUser = await User.findByIdAndUpdate(
         req.params.id,
         {
@@ -34,4 +34,16 @@ export const updateUser = async (req, res, next) => {
     }catch (error) {
         next(error)
     }
-}
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, 'You can only delete your own account!'));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token');
+    res.status(200).json('User has been deleted!')
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -12,6 +12,9 @@ import {
   updateUserStart,
   updateUserSuccess,
   updateUserFailure,
+  deleteUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
 } from "../redux/user/userSlice";
 import { useDispatch } from 'react-redux';
 
@@ -86,78 +89,101 @@ export default function Profile() {
     }
   };
   
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
    return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <input
-          onChange={(e) => setFile(e.target.files[0])}
-          type="file"
-          ref={fileRef}
-          hidden
-          accept="image/*"
-        />
+     <div className="p-3 max-w-lg mx-auto">
+       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+         <input
+           onChange={(e) => setFile(e.target.files[0])}
+           type="file"
+           ref={fileRef}
+           hidden
+           accept="image/*"
+         />
 
-        <img
-          onClick={() => fileRef.current.click()}
-          src={formData.avatar || currentUser.avatar}
-          alt="profile"
-          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
-        />
+         <img
+           onClick={() => fileRef.current.click()}
+           src={formData.avatar || currentUser.avatar}
+           alt="profile"
+           className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+         />
 
-        <p className="text-sm self-center">
-          {fileUploadError ? (
-            <span className="text-red-700">
-              Error Image upload (image must be less than 2 mb)
-            </span>
-          ) : filePerc > 0 && filePerc < 100 ? (
-            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
-          ) : filePerc === 100 ? (
-            <span className="text-green-700">Image successfully uploaded!</span>
-          ) : (
-            ""
-          )}
-        </p>
+         <p className="text-sm self-center">
+           {fileUploadError ? (
+             <span className="text-red-700">
+               Error Image upload (image must be less than 2 mb)
+             </span>
+           ) : filePerc > 0 && filePerc < 100 ? (
+             <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+           ) : filePerc === 100 ? (
+             <span className="text-green-700">
+               Image successfully uploaded!
+             </span>
+           ) : (
+             ""
+           )}
+         </p>
 
-        <input
-          type="text"
-          placeholder="username"
-          defaultValue={currentUser.username}
-          id="username"
-          className="border p-3 rounded-lg"
-          onChange={handlechange}
-        />
-        <input
-          type="email"
-          placeholder="email"
-          defaultValue={currentUser.email}
-          id="email"
-          className="border p-3 rounded-lg"
-          onChange={handlechange}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          id="password"
-          className="border p-3 rounded-lg"
-          onChange={handlechange}
-        />
+         <input
+           type="text"
+           placeholder="username"
+           defaultValue={currentUser.username}
+           id="username"
+           className="border p-3 rounded-lg"
+           onChange={handlechange}
+         />
+         <input
+           type="email"
+           placeholder="email"
+           defaultValue={currentUser.email}
+           id="email"
+           className="border p-3 rounded-lg"
+           onChange={handlechange}
+         />
+         <input
+           type="password"
+           placeholder="password"
+           id="password"
+           className="border p-3 rounded-lg"
+           onChange={handlechange}
+         />
 
-        <button disabled = {loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          {loading? 'loading...' : 'Update'}
-        </button>
-      </form>
+         <button
+           disabled={loading}
+           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+         >
+           {loading ? "loading..." : "Update"}
+         </button>
+       </form>
 
-      <div className="flex justify-between mt-5">
-        <span className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
-      </div>
+       <div className="flex justify-between mt-5">
+         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">
+          Delete Account
+         </span>
+         <span className="text-red-700 cursor-pointer">Sign Out</span>
+       </div>
 
-      <p className='text-red-700 mt-5'>{error ? error: ''}</p>
-      <p className='text-green-700 mt-5'>
-        {updateSuccess ? 'User is updated successfully!' : ''}
-      </p>
-
-    </div>
-  );
+       <p className="text-red-700 mt-5">{error ? error : ""}</p>
+       <p className="text-green-700 mt-5">
+         {updateSuccess ? "User is updated successfully!" : ""}
+       </p>
+     </div>
+   ); 
 }
