1a610a1d3c05102e6392d5cb72f30b698656928a
Ashish Kumar Mishra <ashishrkl31@gmail.com>
2024-04-10 18:07:08 +0530

    Create get user listings api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from "../models/user.model.js";
 import { errorHandler } from '../utils/error.js';
+import Listing from '../models/listing.model.js';
 
 export const test = (req,res) => {
     res.json({
@@ -43,6 +44,19 @@ export const deleteUser = async (req, res, next) => {
     await User.findByIdAndDelete(req.params.id);
     res.clearCookie('access_token');
     res.status(200).json('User has been deleted!')
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  try {
+    if (req.user.id === req.params.id) {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } else {
+      return next(errorHandler(401, 'You can only retrieve your own account listings!'));
+    }
   } catch (error) {
     next(error);
   }
