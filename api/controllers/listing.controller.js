32031f6bc42cfb9c964bded803b8c59a28dc5217
Ashish Kumar Mishra <ashishrkl31@gmail.com>
2024-04-13 01:15:15 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -10,12 +10,14 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
   if (!listing) {
     return next(errorHandler(404, 'Listing not found!'));
   }
+
   if (req.user.id !== listing.userRef) {
     return next(errorHandler(401, 'You can only delete your own listings!'));
   }
