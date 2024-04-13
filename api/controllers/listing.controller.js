6ca5fead5a4040a3e0da2bbf273e2874a689b699
Ashish Kumar Mishra <ashishrkl31@gmail.com>
2024-04-13 22:18:14 +0530

    complete update listing functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -16,6 +16,7 @@ export const deleteListing = async (req, res, next) => {
   if (!listing) {
     return next(errorHandler(404, 'Listing not found!'));
   }
+
   if (req.user.id !== listing.userRef) {
     return next(errorHandler(401, 'You can only delete your own listings!'));
   }
@@ -49,3 +50,16 @@ export const updateListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
