580b8f379ea75545d1ac29418bd801ccfdca91aa
Ashish Kumar Mishra <ashishrkl31@gmail.com>
2024-04-15 12:08:38 +0530

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -303,7 +303,9 @@ export default function CreateListing() {
 
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">($ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
               </div>
             </div>
 
