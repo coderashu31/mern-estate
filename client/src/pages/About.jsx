2e0c744e2a947b3a099be989c6b089f206d44c35
Ashish Kumar Mishra <ashishrkl31@gmail.com>
2024-04-16 21:23:23 +0530

    Pre-installation

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Mishra Estate</h1>
+      <p className='mb-4 text-slate-700'>Mishra Estate stands as a premier real estate agency renowned for its expertise in facilitating seamless property transactions within sought-after neighborhoods. With a seasoned team of professionals at the helm, we specialize in assisting clients with their buying, selling, and renting endeavors. Our commitment lies in delivering unparalleled service, ensuring a smooth and hassle-free experience throughout the entire process. Trust Mishra Estate to navigate you through the intricacies of the real estate market with finesse and dedication.</p>
+      <p className='mb-4 text-slate-700'>
+      At Mishra Estate, our mission is clear: to empower our clients in reaching their real estate aspirations through a combination of expert guidance, tailored support, and a profound grasp of the local real estate landscape. Whether you're in the market to buy, sell, or rent a property, our dedicated team is committed to assisting you at every stage of your journey. Trust us to be your reliable partner as you navigate the dynamic world of real estate, ensuring your goals are not just met, but exceeded.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of seasoned agents brings a wealth of experience and expertise to the real estate arena, dedicated to delivering unparalleled service to our valued clients. We firmly believe that the process of buying or selling a property should be nothing short of exhilarating and fulfilling. With this ethos at our core, we are steadfast in our commitment to ensuring that each client's journey is not only successful but also a source of delight and satisfaction. Trust Mishra Estate to transform your real estate aspirations into a thrilling reality.</p>
+    </div>
   )
 }
